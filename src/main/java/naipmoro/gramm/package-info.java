fd9a0e48dcdc44afe3b7de44f97b200ed75ad0a5
naipmoro <naipmoro@gmail.com>
2018-09-21 01:03:12 -0400

    modify package-info

--- src/main/java/naipmoro/gramm/package-info.java
+++ src/main/java/naipmoro/gramm/package-info.java
@@ -1,18 +1,22 @@
 /**
+ * <p>
  * Gramm is an Antlr-based Metamath proof verifier. From a BNF-like grammar of
  * the Metamath language, Antlr4 creates a parser which, when fed a source
  * file, generates a parse tree.
+ * </p>
  *
  * <table class="image">
- * <caption align="bottom">a small branch of set.mm</caption>
+ * <caption align="bottom">A small branch of set.mm</caption>
  * <tr><td><img src="doc-files/mmexample.png"></td></tr>
  * </table>
  *
- * We walk the tree by extending an empty Antlr4-provided listener. At each
- * significant node of the tree, tokens are gathered and sent to the global
- * {@link naipmoro.gramm.ScopeStack} for processing. The most important
- * processing occurs at the proof nodes and involves calls to
- * {@link naipmoro.gramm.Proof#verify()}.
+ * <p>
+ * We walk the tree by extending an Antlr4-provided empty base listener. At
+ * significant terminal nodes of the tree, tokens are gathered and sent to the
+ * global {@link naipmoro.gramm.ScopeStack} for processing. The most important
+ * processing occurs in response to the {@code proof} nodes and involves calls
+ * to {@link naipmoro.gramm.Proof#verify()}.
+ * </p>
  */
 
 package naipmoro.gramm;
